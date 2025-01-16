8cffb6df22300adc8baa0c26e12b0c5619b0d85b
WildLevko <pastukhovlev@gmail.com>
2025-01-16 17:33:49 +0200

    Add files via upload

--- jsforbank.js
+++ jsforbank.js
@@ -1,1 +1,2 @@
 document.getElementById("spisok2").style.background = "linear-gradient(green, lightgreen, green)";
+document.getElementById("spisok3").style.background = "linear-gradient(red, yellow, red)";
